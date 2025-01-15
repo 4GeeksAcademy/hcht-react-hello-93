61498e67584e2f97b4fdfb14994fb3e16d37fbd6
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-01-15 18:02:10 +0000

    [sp-93] class1: first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,9 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react";  // Import react into the bundle
+import ReactDOM from "react-dom/client";  // Import ReactDOM
+import "../styles/index.css";  // Include your styles into the webpack bundle
+import Home from "./component/Home.jsx";  // Import your own components
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
+// Render your react application
+//                 // donde ?                       // Qué  
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
